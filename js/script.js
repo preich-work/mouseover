97034da95dc7b524dea324a56f9c57ee7d0d105d
preich-work <preich_work@yahoo.com>
2023-08-07 14:00:00 -0400

    Added mouseovers to numbers

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var fans = document.querySelector(".fans");
+fans.addEventListener("mouseover", function ()
+{
+    fans.innerText = "123K";
+}
+);
+var fish = document.querySelector(".fish");
+fish.addEventListener("mouseover", function ()
+{
+    fish.innerText = "47K";
+}
+
+);
+var pets = document.querySelector(".pets");
+pets.addEventListener("mouseover", function ()
+{
+   pets.innerText = "20K"; 
+}
+);
