874baa7b0e420b25981bca062b24411f3088aace
preich-work <preich_work@yahoo.com>
2023-08-07 14:54:47 -0400

    Added toggle light/dark mode

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
 var fans = document.querySelector(".fans");
-fans.addEventListener("mouseover", function ()
-{
-    fans.innerText = "123K";
-}
-);
+fans.addEventListener("mouseover", function () {
+  fans.innerText = "123K";
+});
 var fish = document.querySelector(".fish");
-fish.addEventListener("mouseover", function ()
-{
-    fish.innerText = "47K";
-}
-
-);
+fish.addEventListener("mouseover", function () {
+  fish.innerText = "47K";
+});
 var pets = document.querySelector(".pets");
-pets.addEventListener("mouseover", function ()
-{
-   pets.innerText = "20K"; 
-}
-);
+pets.addEventListener("mouseover", function () {
+  pets.innerText = "20K";
+});
+
+var toggle = document.querySelector("#toggle");
+var ball = document.querySelector(".ball");
+var h1 = document.querySelector("h1");
+var body = document.querySelector("body");
+toggle.addEventListener("click", function () {
+  if (toggle.classList.contains("light")) {
+    toggle.classList.remove("light");
+    h1.innerText = "Stealth Quincy";
+    ball.classList.remove("move-right");
+  } else {
+    body.classList.add("light");
+    ball.classList.add("move-right");
+    h1.innerText = "Party Quincy";
+  }
+});
